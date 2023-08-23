8b51831e122d4f0e8eac563a1433fe45691fcf81
Alexander Korobov <dktwho@mail.ru>
2023-08-23 12:32:41 +0300

    add test for userReducer with two test

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -12,8 +12,11 @@ type ActionType = {
 
 export const userReducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
-        case '1': {
-            return state
+        case 'INCREMENT-AGE': {
+            return {...state, age: state.age + 1}
+        }
+        case 'INCREMENT-CHILDREN-COUNT': {
+            return {...state, childrenCount: state.childrenCount + 1}
         }
         default: {
             return state
